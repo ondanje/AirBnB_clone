9d297787dc1205458cf2936f04bcc9357c98a7b7
ondanje <cyrilondanje@gmail.com>
2023-10-16 00:06:22 +0300

    user correction

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel"""
 
-    email = " "
-    password = " "
-    first_name = " "
-    last_name = " "
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
