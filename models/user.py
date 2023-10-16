e13d87b958a4756fae35cd1d8ad6efc0d8c2ca19
Atala2000 <atalakidi@gmail.com>
2023-10-16 17:02:44 +0300

    Changes to init

--- models/user.py
+++ models/user.py
@@ -8,6 +8,10 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """User class that inherits from BaseModel"""
 
+    def __init__(self, *args, **kwargs):
+        """Initializes User"""
+        super().__init__(*args, **kwargs)
+
     email = ""
     password = ""
     first_name = ""
