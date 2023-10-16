73503a9aa3f4740aa66f4ce6b197bdedcf868f36
Atala2000 <atalakidi@gmail.com>
2023-10-16 16:54:20 +0300

    Changes to init

--- models/user.py
+++ models/user.py
@@ -13,6 +13,6 @@ class User(BaseModel):
     first_name = ""
     last_name = ""
 
-    def init(self, args, **kwargs):
+    def __init__(self, args, **kwargs):
         """initializes User"""
         super().init(args, **kwargs)
